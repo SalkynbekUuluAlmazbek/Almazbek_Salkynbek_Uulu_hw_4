e541c484b2fa588f428b54a3d6b78ab448ab7a98
SalkynbekUuluAlmazbek <salkynbekovalmaz@gmail.com>
2023-03-10 20:15:05 +0600

    new_version

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,24 @@ public class Main {
     public static String bossDefence;
     public static int medicHealth = 220;
     public static int medicHeal = 10;
-    public static int[] heroesHealth = {270, 260, 250,medicHealth};
-    public static int[] heroesDamage = {10, 15, 20, 0};
-    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic","Medical"};
+    public static int golemHealth = 300;
+    public static int golemDamage = 5;
+    public static String golemAttackType = "Physical";
+    public static int luckyHealth = 200;
+    public static double luckyAvoidProbability = 0.2;
+    public static int[] heroesHealth = {270, 260, 250, medicHealth, golemHealth, luckyHealth};
+    public static int[] heroesDamage = {10, 15, 20, 0, golemDamage, 0};
+    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic", "Medical",
+            golemAttackType, "Dodge"};
+
     public static int roundNumber = 0;
     public static String message = "";
+
+    public static boolean isLuckyDodges() {
+        Random random = new Random();
+        double chance = random.nextDouble();
+        return chance < luckyAvoidProbability;
+    }
 
     public static void main(String[] args) {
         printStatistics();
@@ -27,6 +40,7 @@ public class Main {
         heroesHit();
         medicHeal();
         printStatistics();
+
     }
 
     public static void chooseBossDefence() {
@@ -38,10 +52,29 @@ public class Main {
     public static void bossHits() {
         for (int i = 0; i < heroesHealth.length; i++) {
             if (heroesHealth[i] > 0) {
-                if (heroesHealth[i] - bossDamage < 0) {
-                    heroesHealth[i] = 0;
+                if (heroesAttackType[i].equals(golemAttackType)) {
+                    int golemDamageTaken = bossDamage / 5;
+                    if (golemHealth - golemDamageTaken < 0) {
+                        golemHealth = 0;
+                    } else {
+                        golemHealth = golemHealth - golemDamageTaken;
+                    }
+                } else if (heroesAttackType[i].equals("Dodge")) {
+                    if (isLuckyDodges()) {
+                        message = "Lucky dodged boss's attack!";
+                    } else {
+                        if (heroesHealth[i] - bossDamage < 0) {
+                            heroesHealth[i] = 0;
+                        } else {
+                            heroesHealth[i] = heroesHealth[i] - bossDamage;
+                        }
+                    }
                 } else {
-                    heroesHealth[i] = heroesHealth[i] - bossDamage;
+                    if (heroesHealth[i] - bossDamage < 0) {
+                        heroesHealth[i] = 0;
+                    } else {
+                        heroesHealth[i] = heroesHealth[i] - bossDamage;
+                    }
                 }
             }
         }
@@ -65,23 +98,27 @@ public class Main {
             }
         }
     }
+
     public static void medicHeal() {
 
-            if (medicHealth > 0) {
-                int minHealth = heroesHealth[0];
-                int minIndex = 0;
-                for (int i = 1; i < heroesHealth.length - 1; i++) {
-                    if (heroesHealth[i] < minHealth && heroesHealth[i] > 0 && heroesHealth[i] < 100) {
-                        minHealth = heroesHealth[i];
-                        minIndex = i;
-                    }
-                }
-                if (minHealth < 100) {
-                    heroesHealth[minIndex] += medicHeal;
-
+        if (medicHealth > 0) {
+            int minHealth = heroesHealth[0];
+            int minIndex = 0;
+            for (int i = 1; i < heroesHealth.length - 1; i++) {
+                if (heroesHealth[i] < minHealth && heroesHealth[i] > 0 && heroesHealth[i] < 100) {
+                    minHealth = heroesHealth[i];
+                    minIndex = i;
                 }
             }
+            if (minHealth < 100) {
+                heroesHealth[minIndex] += medicHeal;
+                if (medicHealth <= 0) {
+                    System.out.println("Medic is dead and can no longer heal.");
+                }
+
+            }
         }
+    }
 
 
     public static boolean isGameFinished() {
@@ -89,38 +126,39 @@ public class Main {
             System.out.println("Heroes won!!!");
             return true;
         }
-        /*if (heroesHealth[0] <= 0 && heroesHealth[1] <= 0 && heroesHealth[2] <= 0) {
-            System.out.println("Boss won!!!");
-            return true;
-        }
-        return false;*/
         boolean allHeroesDead = true;
         for (int i = 0; i < heroesHealth.length; i++) {
-            if (heroesHealth[i] > 0) {
+            if (heroesHealth[i] > 0 && !heroesAttackType[i].equals(golemAttackType)) {
                 allHeroesDead = false;
                 break;
             }
         }
-        if (allHeroesDead) {
+        if (allHeroesDead || bossHealth <= 0) {
             System.out.println("Boss won!!!");
         }
         return allHeroesDead;
     }
 
+
     public static void printStatistics() {
         System.out.println("ROUND " + roundNumber + " ----------");
-        /*String defence;
-        if (bossDefence == null) {
-            defence = "No defence";
-        } else {
-            defence = bossDefence;
-        }*/
         System.out.println("Boss health: " + bossHealth + " damage: " + bossDamage
                 + " defence: " + (bossDefence == null ? "No defence" : bossDefence));
         for (int i = 0; i < heroesHealth.length; i++) {
-            System.out.println(heroesAttackType[i] + " health: " + heroesHealth[i]
-                    + " damage: " + heroesDamage[i]);
+            if (heroesAttackType[i].equals(golemAttackType)) {
+                System.out.println("Golem health: " + golemHealth
+                        + " damage: " + golemDamage);
+            }
+            if (heroesAttackType[i].equals("Dodge")) {
+                System.out.println("Lucky health: " + heroesHealth[i]);
+            } else {
+                System.out.println(heroesAttackType[i] + " health: " + heroesHealth[i]
+                        + " damage: " + heroesDamage[i]);
+            }
+
+
         }
         System.out.println(message);
     }
+
 }
